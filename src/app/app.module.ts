cabe984a695d03e8855da978c370d16177469eca
Ivan Cassano <ivan.cassano12@gmail.com>
2019-07-18 17:34:49 -0300

    Folder structure + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { PublicModule } from './public/public.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PublicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
