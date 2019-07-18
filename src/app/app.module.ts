ae15f9ab0442d15119fa3b2fe879b5d83d0af7c8
Ivan Cassano <ivan.cassano12@gmail.com>
2019-07-18 18:49:57 -0300

    First working call to a city details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { PublicModule } from './public/public.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     PublicModule
   ],
---
+++ src/app/core/services/weather-api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherApiService {
+  private url = 'https://api.openweathermap.org/data/2.5/weather';
+  private params = new HttpParams().set('APPID', 'fee18f34c8778a20c14306ed2d9e34a9');
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getWeather(): Observable<any> {
+    const auxParams = this.params.set('q', 'London');
+    return this.http.get(this.url, {params: auxParams});
+  }
+}
